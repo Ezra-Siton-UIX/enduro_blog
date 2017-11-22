8f52d8a498d84e747522cccc1fbcbca6fa216cdd
Your Name <you@example.com>
2017-11-23 00:34:52 +0200

    first

---
+++ cms/.users.js
@@ -0,0 +1,11 @@
+{
+	users: [
+		{
+			username: 'test',
+			tags: [],
+			salt: 'b4b0e88a8aabf66e6b0de6268245e79c',
+			hash: '5d092682d941006b59f8ea6225f3cf9c16b382aa67d388be65f5ffa6a8e81027',
+			user_created_timestamp: 1511389167688
+		}
+	]
+}
